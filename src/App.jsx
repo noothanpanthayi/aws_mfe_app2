1ad1b3da7e33610a0986b98b772dd5b268b4646f
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-18 14:45:06 -0600

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+
+        <div>React Application 1</div>
+        <div>Url:</div>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
