9a99d4e72813b29c6f52a32d74f553888ec5affa
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-29 20:02:13 -0600

    update2

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,14 @@ export default defineConfig({
     origin: 'http://3.83.250.40',  
     methods: ['GET', 'POST', 'OPTIONS'], 
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'] 
+    },
+    middlewareMode: 'html',
+    configureServer: (server) => { 
+      server.middlewares.use((req, res, next) => { 
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization'); next(); 
+      });
     }
-    }
+  }
 })
